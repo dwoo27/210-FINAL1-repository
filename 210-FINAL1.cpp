c0b7632645f3cef8be066ddaf5dac2ab496bb98a
dwoo27 <dwooed27@gmail.com>
2026-05-20 18:16:51 -0700

    10 min commit

--- 210-FINAL1.cpp
+++ 210-FINAL1.cpp
@@ -1,8 +1,28 @@
-
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <map>
+using namespace std;
 
 int main()
 {
-  
+	map<string, int> traffic;
+	
+	ifstream fin;
+	fin.open("210-final-1-DP26.txt");
+
+	string origin;
+	string destination;
+	string buf;
+
+	if (fin.good()){
+		while (getline(fin, buf)) {
+			origin = buf.substr(0, 3);
+			destination = buf.substr(4, 6);
+
+		}
+
+	}
+
 }
 
